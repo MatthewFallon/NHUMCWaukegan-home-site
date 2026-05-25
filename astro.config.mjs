913ca4cbe6432c94ea80aee7562a916f92934a41
Matthew Fallon <matthew.b.fallon@gmail.com>
2026-05-24 20:22:59 -0500

    switch to astro-decap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,31 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import DecapCMS from "@jee-r/astro-decap-cms";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        DecapCMS({
+            config: {
+                backend: {
+                    name: 'git-gateway',
+                    branch: 'main',
+                },
+                collections: [
+                    // Define a blog post collection
+                    {
+                        name: 'posts',
+                        label: 'Blog Posts',
+                        folder: 'src/pages/posts',
+                        create: true,
+                        delete: true,
+                        fields: [
+                            { name: 'title', widget: 'string', label: 'Post Title' },
+                            { name: 'body', widget: 'markdown', label: 'Post Body' },
+                        ],
+                    },
+                ],
+            },
+        }),
+    ],
+});
