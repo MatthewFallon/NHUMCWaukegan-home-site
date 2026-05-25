4992684726cba9b31f3392c69b1bdf9d4151aeaf
Matthew Fallon <matthew.b.fallon@gmail.com>
2026-05-24 22:13:38 -0500

    Setting the image folders for decap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ export default defineConfig({
     integrations: [
         DecapCMS({
             config: {
+                media_folder: "/public/assets/images",// Media files will be stored in the repo under static/images/uploads
+                public_folder: "/assets/images", // The src attribute for uploaded media will begin with /images/uploads
                 backend: {
                     name: 'git-gateway',
                     branch: 'main',
